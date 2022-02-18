061e5b4b0638c57fa304f4bbb073d535f9ead5e7
Felipe Hertzer <felipeaugustohertzer@gmail.com>
2022-02-18 19:15:55 +1100

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+import sys
+
+extra = {}
+if sys.version_info >= (3, 0):
+    extra.update(use_2to3=True)
+
+setup(
+    name='mailer',
+    version="0.8.1",
+    description="A module to send email simply in Python",
+    author="Felipe Hertzer and Ryan Ginstrom",
+    author_email='felipe@hertzer.net',
+    url="https://github.com/felipehertzer/mailer",
+    py_modules=["mailer"],
+    keywords=["email", "smtp"],
+    license="MIT License",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX",
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ],
+    **extra
+)
